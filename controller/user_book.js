d9ac2602354d35859525b15c2366b72803d8963e
vanhung1999 <vanhung1999dev@gmail.com>
2020-03-18 20:23:13 +0700

    done

--- controller/user_book.js
+++ controller/user_book.js
@@ -1,13 +1,15 @@
 const User = require('../model/user');
+const Promise = require('bluebird');
 const Seneca = require('seneca')();
+const act = Promise.promisify(Seneca.act,{context: Seneca});
 Seneca.quiet()
     .use(require('../service/user_service'))
     .use(require('../service/book_service'));
 
 module.exports.GetAll = async (req, res) => {
     try {
-        const book = await Seneca.act({ role: 'book', cmd: 'getAll' });
-        const uesr = await Seneca.act({ role: 'user', cmd: 'getAll' });
+        const book = await act({ role: 'book', cmd: 'getAll' });
+        const uesr = await act({ role: 'user', cmd: 'getAll' });
         res.send({user: uesr,book: book});
     } catch (error) {
         console.log(error);
