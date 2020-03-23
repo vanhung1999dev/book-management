19cd0114fda750632d9d8bf036195f870bdb5d8f
vanhung1999 <vanhung1999dev@gmail.com>
2020-03-23 10:23:55 +0700

    add account permison

--- constant/path.js
+++ constant/path.js
@@ -1,31 +1,41 @@
 
-const list_permision= [
-    //account permision
-    {path:"^POST,/users$",id_permision:1},
-    {path:"^GET,/users/[0-9]+$",id_permision:2},
-    {path:"^GET,/users$",id_permision:3},
-    {path:"^PUT,/users/[0-9]+$",id_permision:4},
-    {path:"^DELETE,/users/[0-9]+$",id_permision:5},
+const list_permision = [
+    //user permision
+    { path: "^POST,/users$", id_permision: 1 },
+    { path: "^GET,/users/[0-9]+$", id_permision: 2 },
+    { path: "^GET,/users$", id_permision: 3 },
+    { path: "^PUT,/users/[0-9]+$", id_permision: 4 },
+    { path: "^DELETE,/users/[0-9]+$", id_permision: 5 },
 
     //author permision
-    {path:"^POST,/authors$",id_permision:6},
-    {path:"^GET,/authors/[0-9]+$",id_permision:7},
-    {path:"^GET,/authors$",id_permision:8},
-    {path:"^PUT,/authors/[0-9]+$",id_permision:9},
-    {path:"^DELETE,/authors/[0-9]+$",id_permision:10},
+    { path: "^POST,/authors$", id_permision: 6 },
+    { path: "^GET,/authors/[0-9]+$", id_permision: 7 },
+    { path: "^GET,/authors$", id_permision: 8 },
+    { path: "^PUT,/authors/[0-9]+$", id_permision: 9 },
+    { path: "^DELETE,/authors/[0-9]+$", id_permision: 10 },
 
     //book permision 
-    {path:"^POST,/books$",id_permision:11},
-    {path:"^GET,/books/[0-9]+$",id_permision:12},
-    {path:"^GET,/books$",id_permision:13},
-    {path:"^PUT,/books/[0-9]+$",id_permision:14},
-    {path:"^DELETE,/books/[0-9]+$",id_permision:15},
+    { path: "^POST,/books$", id_permision: 11 },
+    { path: "^GET,/books/[0-9]+$", id_permision: 12 },
+    { path: "^GET,/books$", id_permision: 13 },
+    { path: "^PUT,/books/[0-9]+$", id_permision: 14 },
+    { path: "^DELETE,/books/[0-9]+$", id_permision: 15 },
 
     //catelog permision
-    {path:"^POST,/catelogs$",id_permision:16},
-    {path:"^GET,/catelogs/[0-9]+$",id_permision:17},
-    {path:"^GET,/catelogs$",id_permision:18},
-    {path:"^PUT,/catelogs/[0-9]+$",id_permision:19},
-    {path:"^DELETE,/catelogs/[0-9]+$",id_permision:20}
+    { path: "^POST,/catelogs$", id_permision: 16 },
+    { path: "^GET,/catelogs/[0-9]+$", id_permision: 17 },
+    { path: "^GET,/catelogs$", id_permision: 18 },
+    { path: "^PUT,/catelogs/[0-9]+$", id_permision: 19 },
+    { path: "^DELETE,/catelogs/[0-9]+$", id_permision: 20 },
+
+    //account permision
+    { path: "^POST,/accounts$", id_permision: 21 },
+    { path: "^GET,/accounts/[0-9]+$$", id_permision: 22 },
+    { path: "^GET,/accounts$", id_permision: 23 },
+    { path: "^PUT,/accounts/[0-9]+$$", id_permision: 24 },
+    { path: "^PUT,/accounts/[0-9]+$$", id_permision: 25 },
+
+
+
 ]
 module.exports = list_permision;
