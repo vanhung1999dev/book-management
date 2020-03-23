bbe0b73cd18dc7b552af1c6bf94ec6b15bc58ceb
vanhung1999 <vanhung1999dev@gmail.com>
2020-03-23 10:20:49 +0700

    add router account

--- router/admin.js
+++ router/admin.js
@@ -1,4 +1,5 @@
 const user_controller = require('../controller/user');
+const account_controller = require('../controller/account');
 const Express = require('express');
 const router = Express.Router();
 const Middleware = require('../middleware/login');
@@ -9,5 +10,11 @@ router.get('/users/:id', user_controller.Get);
 router.get('/users', user_controller.GetAll);
 router.put('/users/:id', user_controller.Update);
 router.delete('/users/:id', user_controller.Delete);
+
+router.post('/accounts', account_controller.MakeAccount);
+router.get('/accounts/:id',account_controller.GetAccount);
+router.get('/accounts', account_controller.GetAccounts);
+router.put('/accounts/:id', account_controller.UpdateAccount);
+router.put('/accounts/:id',account_controller.BlockAccount);
 
 module.exports = router;
