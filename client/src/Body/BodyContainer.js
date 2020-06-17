cb6d072f26d6160b5e07bc98146cae278b60474e
vanhung1999 <vanhung1999dev@gmail.com>
2020-06-17 15:59:51 +0700

    fix path to filter book

--- client/src/Body/BodyContainer.js
+++ client/src/Body/BodyContainer.js
@@ -7,23 +7,31 @@ function BodyContainer(props) {
     const { selectedItem, setSelectedItem, filter, setFilter } = props;
 
     const getBooks = async () => {
-        const { data } = await Axios.get(`http://localhost:3001/books`, {
+        const { data } = await Axios.get(`http://localhost:3001/books?name=${selectedItem}&&title=${filter}`, {
             headers: {
                 authorization: `bearer ${localStorage.getItem('jwt')}`
             }
         });
         setBooks(data);
+        console.log('name catalog:',selectedItem);
+        console.log('filter',filter);
+        console.log('Book :', data);
     }
 
     useEffect(() => {
         getBooks();
-    }, [selectedItem,filter]);
+    }, [selectedItem, filter]);
 
-    return (
-        <div>
-            {books.map(book => <h1 key={book.isbn}>{book.title}  {book.description}</h1>)}
-        </div>
-    )
+    if (books.length === 0)
+        return (
+            <div>No</div>
+        )
+    else
+        return (
+            <div>
+                {books.map(book => <h1 key={book.isbn}>{book.title}  {book.description}</h1>)}
+            </div>
+        )
 }
 
 export default BodyContainer
